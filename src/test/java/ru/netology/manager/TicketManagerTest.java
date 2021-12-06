28e925ba33c9910624f24f35f5a467920db56093
KseniyaChepelevich <chepelevich.qamid.5@gmail.com>
2021-12-06 11:16:00 +0300

    added TicketByTravelTimeComparator

--- src/test/java/ru/netology/manager/TicketManagerTest.java
+++ src/test/java/ru/netology/manager/TicketManagerTest.java
@@ -2,11 +2,13 @@ package ru.netology.manager;
 
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Ticket;
+import ru.netology.domain.TicketByTravelTimeComparator;
 import ru.netology.repository.TicketRepository;
 
-import java.util.Arrays;
+import java.util.Comparator;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 
 class TicketManagerTest {
     private TicketRepository repository = new TicketRepository();
@@ -17,6 +19,8 @@ class TicketManagerTest {
     private Ticket fromMoscowToPiterMorningDMELED = new Ticket(3, 1750, "DME", "LED", 110);
     private Ticket fromMoscowToPiterEveningDMELED = new Ticket(4,2250, "DME", "LED", 100);
     private Ticket fromMoscowToPiterEveningDMELED2 = new Ticket(14, 2700, "DME", "LED", 120);
+    private Ticket fromMoscowToPiterEveningDMELED3 = new Ticket(16, 2700, "DME", "LED", 100);
+    private Ticket fromMoscowToPiterEveningDMELED4 = new Ticket(17, 2250, "DME", "LED", 130);
     private Ticket FromMoscowToPiterEveningVKOLED = new Ticket(5, 2000, "VKO", "LED", 90);
     private Ticket FromMoscowToKazanMorningVKOLED = new Ticket(6, 2600, "VKO", "KZN", 95);
     private Ticket FromMoscowToKazanMorningSVOKZN = new Ticket(7, 3700, "SVO", "KZN", 600);
@@ -27,7 +31,7 @@ class TicketManagerTest {
     private Ticket fromPiterToMoscowEveningLEDDME = new Ticket(12, 2500, "LED", "DME", 90);
     private Ticket fromPiterToMoscowMorningLEDDME = new Ticket(13, 2700, "LED", "DME", 80);
     private Ticket FromMoscowToKazanMorningDMEKZN = new Ticket(15, 2000, "DME", "KZN", 120);
-
+    private Comparator<Ticket> TicketByTravelTimeComparator;
 
 
     @Test
@@ -47,7 +51,7 @@ class TicketManagerTest {
         Ticket[] expected = new Ticket[]{fromMoscowToPiterMorningDMELED, fromMoscowToPiterEveningDMELED, fromMoscowToPiterEveningDMELED2};
         Ticket[] actual = manager.search(fromAirport, toAirport);
 
-        Arrays.sort(actual);
+
 
         assertArrayEquals(expected, actual);
     }
@@ -68,7 +72,7 @@ class TicketManagerTest {
         Ticket[] expected = new Ticket[]{fromMoscowToPiterMorningDMELED, fromMoscowToPiterEveningDMELED, fromMoscowToPiterEveningDMELED2};
         Ticket[] actual = manager.search(fromAirport, toAirport);
 
-        Arrays.sort(actual);
+
 
         assertArrayEquals(expected, actual);
     }
@@ -89,7 +93,7 @@ class TicketManagerTest {
         Ticket[] expected = new Ticket[]{fromMoscowToPiterMorningSVOLED};
         Ticket[] actual = manager.search(fromAirport, toAirport);
 
-        Arrays.sort(actual);
+
 
         assertArrayEquals(expected, actual);
     }
@@ -110,9 +114,88 @@ class TicketManagerTest {
         Ticket[] expected = new Ticket[]{};
         Ticket[] actual = manager.search(fromAirport, toAirport);
 
-        Arrays.sort(actual);
+
 
         assertArrayEquals(expected, actual);
     }
+
+    @Test
+    public void shouldSearch4TicketsOutOf7WithTheSameAirportNamesAndSortByTravelTime() {
+        String fromAirport = "DME";
+        String toAirport = "LED";
+
+        manager.add(fromPiterToMoscowMorningLEDDME);
+        manager.add(fromMoscowToPiterEveningDMELED2);
+        manager.add(fromPiterToMoscowEveningLEDDME);
+        manager.add(fromMoscowToPiterMorningDMELED);
+        manager.add(fromMoscowToPiterEveningDMELED);
+        manager.add(fromMoscowToPiterEveningDMELED3);
+        manager.add(fromMoscowToPiterEveningDMELED4);
+
+
+        Ticket[] expected = new Ticket[]{ fromMoscowToPiterEveningDMELED, fromMoscowToPiterEveningDMELED3, fromMoscowToPiterMorningDMELED, fromMoscowToPiterEveningDMELED2, fromMoscowToPiterEveningDMELED4 };
+        Ticket[] actual = manager.findAll(fromAirport, toAirport, TicketByTravelTimeComparator);
+
+
+
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearch2TicketsOutOf3WithTheSameAirportNamesAndSortByTravelTime() {
+        String fromAirport = "DME";
+        String toAirport = "LED";
+
+        manager.add(fromPiterToMoscowMorningLEDDME);
+        manager.add(fromMoscowToPiterEveningDMELED2);
+        manager.add(fromMoscowToPiterMorningDMELED);
+
+
+
+        Ticket[] expected = new Ticket[]{ fromMoscowToPiterMorningDMELED, fromMoscowToPiterEveningDMELED2};
+        Ticket[] actual = manager.findAll(fromAirport, toAirport, TicketByTravelTimeComparator);
+
+
+
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearch1TicketsOutOf1WithTheSameAirportNamesAndSortByTravelTime() {
+        String fromAirport = "DME";
+        String toAirport = "LED";
+
+
+        manager.add(fromMoscowToPiterEveningDMELED2);
+
+
+        Ticket[] expected = new Ticket[]{ fromMoscowToPiterEveningDMELED2};
+        Ticket[] actual = manager.findAll(fromAirport, toAirport, TicketByTravelTimeComparator);
+
+
+
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearch1TicketsIfEmptyWithTheSameAirportNamesAndSortByTravelTime() {
+        String fromAirport = "DME";
+        String toAirport = "LED";
+
+
+
+
+        Ticket[] expected = new Ticket[]{ };
+        Ticket[] actual = manager.findAll(fromAirport, toAirport, TicketByTravelTimeComparator);
+
+
+
+
+        assertArrayEquals(expected, actual);
+    }
+
 
 }
